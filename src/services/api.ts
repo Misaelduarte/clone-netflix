0c16009cd3916d4be6006b622f66eb98a8218563
Misael Duarte <developer.md775@gmail.com>
2022-02-01 01:47:28 -0300

    changes api tmdb

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,5 +1,8 @@
 import axios from "axios";
 
+// REACT_APP_API_KEY_TMDB=f0dfd19a0bb045afd092170f61e78676
+// REACT_APP_API_BASE=https://api.themoviedb.org/3
+
 const api = axios.create({
   baseURL: 'https://api.themoviedb.org/3'
 })
